6f77db13579722d646d26c28f72d8b8d10edba94
DmitryMalomoshin <makropulas@gmail.com>
2023-04-19 02:03:01 +0300

    Add DZ7

---
+++ src/DZ_OOP_Part3(Python)/view.py
@@ -0,0 +1,10 @@
+class View:
+
+    def view_data(data, title):
+        print(f'{title}: {data}')
+
+    def get_value():
+        return int(input('Введите число: '))
+
+    def get_operator():
+        return str(input('Введите оператор: '))
